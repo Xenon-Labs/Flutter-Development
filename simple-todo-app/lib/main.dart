acce45b72a219d210b88fab2c0d8d064851047e7
Shivanouyen Yoganathan <shivanouyen.yoganathan@uoit.net>
2019-12-29 23:21:33 -0500

    Linting

--- simple-todo-app/lib/main.dart
+++ simple-todo-app/lib/main.dart
@@ -1,50 +1,33 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(new App());
+void main() => runApp(App());
 
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(title: 'To-Do List', home: new TodoList());
+    return MaterialApp(title: 'To-Do List', home: TodoList());
   }
 }
 
 class TodoList extends StatefulWidget {
   @override
-  _TodoListState createState() => new _TodoListState();
+  _TodoListState createState() => _TodoListState();
 }
 
 class _TodoListState extends State<TodoList> {
-  List<String> _todoList = [];
-  TextEditingController _textFieldController = TextEditingController();
+  final List<String> _todoList = [];
+  final TextEditingController _textFieldController = TextEditingController();
 
-  _displayDialog(BuildContext context) async {
-    return showDialog(
-        context: context,
-        builder: (context) {
-          return AlertDialog(
-            title: Text('Add a task to your list'),
-            content: TextField(
-              controller: _textFieldController,
-              decoration: InputDecoration(hintText: "Enter task here"),
-            ),
-            actions: <Widget>[
-              new FlatButton(
-                child: new Text('ADD'),
-                onPressed: () {
-                  Navigator.of(context).pop();
-                  _addTodoItem(_textFieldController.text);
-                },
-              ),
-              new FlatButton(
-                child: new Text('CANCEL'),
-                onPressed: () {
-                  Navigator.of(context).pop();
-                },
-              )
-            ],
-          );
-        });
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('To-Do List')),
+      body: ListView(children: _getItems()),
+      floatingActionButton: FloatingActionButton(
+          onPressed: () => _displayDialog(context),
+          tooltip: 'Add Item',
+          child: Icon(Icons.add)),
+    );
   }
 
   void _addTodoItem(String title) {
@@ -57,28 +40,45 @@ class _TodoListState extends State<TodoList> {
   }
 
   // Generate list of item widgets
+  Widget _buildTodoItem(String title) {
+    return ListTile(title: Text(title));
+  }
+
+  // Generate a single item widget
+  Future<AlertDialog> _displayDialog(BuildContext context) async {
+    return showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: const Text('Add a task to your list'),
+            content: TextField(
+              controller: _textFieldController,
+              decoration: const InputDecoration(hintText: 'Enter task here'),
+            ),
+            actions: <Widget>[
+              FlatButton(
+                child: const Text('ADD'),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                  _addTodoItem(_textFieldController.text);
+                },
+              ),
+              FlatButton(
+                child: const Text('CANCEL'),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              )
+            ],
+          );
+        });
+  }
+
   List<Widget> _getItems() {
-    List<Widget> _todoWidgets = [];
+    final List<Widget> _todoWidgets = <Widget>[];
     for (String title in _todoList) {
       _todoWidgets.add(_buildTodoItem(title));
     }
     return _todoWidgets;
-  }
-
-  // Generate a single item widget
-  Widget _buildTodoItem(String title) {
-    return new ListTile(title: new Text(title));
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(title: new Text('To-Do List')),
-      body: ListView(children: _getItems()),
-      floatingActionButton: new FloatingActionButton(
-          onPressed: () => _displayDialog(context),
-          tooltip: 'Add Item',
-          child: new Icon(Icons.add)),
-    );
   }
 }
