d62702278e1a05b82baed4a3dd8cb76013957aff
Arvinth Vijayanathan <arvinth3@hotmail.com>
2020-01-09 11:05:37 -0500

    Add contacts list view

---
+++ contacts_app/lib/contactsPage.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:contacts_service/contacts_service.dart';
+
+class ContactsPage extends StatefulWidget {
+  @override
+  _ContactsPageState createState() => _ContactsPageState();
+}
+
+class _ContactsPageState extends State<ContactsPage> {
+  Iterable<Contact> _contacts;
+
+  @override
+  void initState() {
+    getContacts();
+    super.initState();
+  }
+
+  Future<void> getContacts() async {
+    //We already have permissions for contact when we get to this page, so we
+    // are now just retrieving it
+    final Iterable<Contact> contacts = await ContactsService.getContacts();
+    setState(() {
+      _contacts = contacts;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: (Text('Contacts')),
+      ),
+      body: _contacts != null
+          //Build a list view of all contacts, displaying their avatar and
+          // display name
+          ? ListView.builder(
+              itemCount: _contacts?.length ?? 0,
+              itemBuilder: (BuildContext context, int index) {
+                Contact contact = _contacts?.elementAt(index);
+                return ListTile(
+                  contentPadding:
+                      const EdgeInsets.symmetric(vertical: 2, horizontal: 18),
+                  leading: (contact.avatar != null && contact.avatar.isNotEmpty)
+                      ? CircleAvatar(
+                          backgroundImage: MemoryImage(contact.avatar),
+                        )
+                      : CircleAvatar(
+                          child: Text(contact.initials()),
+                          backgroundColor: Theme.of(context).accentColor,
+                        ),
+                  title: Text(contact.displayName ?? ''),
+                  //This can be further expanded to showing contacts detail
+                  // onPressed().
+                );
+              },
+            )
+          : Center(child: const CircularProgressIndicator()),
+    );
+  }
+}
