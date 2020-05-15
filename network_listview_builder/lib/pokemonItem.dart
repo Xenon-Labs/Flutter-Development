37d61be69a57d082a45c3a2ceb4bf815bff22c8d
Arvinth Vijayanathan <arvinth3@hotmail.com>
2020-05-14 21:13:28 -0400

    Implementation

---
+++ network_listview_builder/lib/pokemonItem.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+import 'package:network_listview_builder/pokemonModel.dart';
+
+class PokemonItem extends StatefulWidget {
+  PokemonItem({Key key, @required this.pokemon}) : super(key: key);
+
+  final Pokemon pokemon;
+
+  @override
+  _PokemonItemState createState() => _PokemonItemState();
+}
+
+class _PokemonItemState extends State<PokemonItem> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.fromLTRB(15, 0, 15, 0),
+      child: Column(
+        children: <Widget>[
+          Row(
+            children: <Widget>[
+              Expanded(
+                child: Column(
+                  children: <Widget>[
+                    Row(
+                      children: <Widget>[
+                        Text(widget.pokemon.name,
+                            style: TextStyle(fontSize: 18))
+                      ],
+                    ),
+                    Row(
+                      children: <Widget>[
+                        Text('height: ' + widget.pokemon.height.toString())
+                      ],
+                    ),
+                    getAbilitiesRow(),
+                    getStatsRows(),
+                  ],
+                ),
+              ),
+              Expanded(
+                  child: Column(
+                children: <Widget>[
+                  Center(
+                    child: Image.network(widget.pokemon.defaultImage),
+                  )
+                ],
+              ))
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Divider(),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget getStatsRows() {
+    List<Widget> statWidgets = <Widget>[];
+
+    for (Map<String, dynamic> stats in widget.pokemon.stats) {
+      statWidgets.add(Row(
+        children: <Widget>[
+          Text(stats['stat']['name'] + ": " + stats['base_stat'].toString()),
+        ],
+      ));
+    }
+
+    return Column(children: statWidgets);
+  }
+
+  Widget getAbilitiesRow() {
+    String abilitiesString = '';
+
+    for (Map<String, dynamic> abilities in widget.pokemon.abilities) {
+      abilitiesString = abilitiesString + abilities['ability']['name'] + ', ';
+    }
+    abilitiesString = abilitiesString.substring(0, abilitiesString.length - 2);
+
+    return Row(children: <Widget>[
+      Expanded(child: Text('abilities: ' + abilitiesString))
+    ]);
+  }
+}
---
+++ network_listview_builder/lib/pokemonNotifier.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:network_listview_builder/pokemonModel.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class PokemonNotifier extends ValueNotifier<List<Pokemon>> {
+  PokemonNotifier() : super(null);
+
+  int _pageNumber = 1;
+  bool _hasMorePokemon = true;
+  int _batchesOf = 5;
+  final String _apiUrl = 'https://pokeapi.co/api/v2/pokemon/';
+  List<Pokemon> _listPokemons;
+
+  @override
+  List<Pokemon> get value => _value;
+  List<Pokemon> _value;
+  @override
+  set value(List<Pokemon> newValue) {
+    _value = newValue;
+    notifyListeners();
+  }
+
+  Future<void> reload() async {
+    _listPokemons = <Pokemon>[];
+    _pageNumber = 1;
+    await httpGetPokemon(_pageNumber);
+  }
+
+  Future<void> getMore() async {
+    if (_hasMorePokemon) {
+      await httpGetPokemon(_pageNumber);
+    }
+  }
+
+  Future<void> httpGetPokemon(int page) async {
+    _listPokemons ??= <Pokemon>[];
+
+    for (var i = 0; i < _batchesOf; i++) {
+      http.Response res = await http.get(_apiUrl + (page + i).toString());
+
+      dynamic jsonDecoded = json.decode(res.body);
+
+      //todo test
+      if (jsonDecoded != null) {
+        _listPokemons.add(Pokemon.fromJson(jsonDecoded));
+      } else {
+        _hasMorePokemon = false;
+        break;
+      }
+    }
+    _pageNumber += _batchesOf;
+
+    value = _listPokemons;
+  }
+}
